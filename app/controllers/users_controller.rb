b1d7036ec9db28488932c75228c31745090fef38
David Wu <david204code@gmail.com>
2020-10-10 21:25:09 +0100

    Add the User index, show and creation action with user_params AND skip_before_action :verify_authenticity_token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+
+    if user
+      render json: {
+        user: user,
+      }
+    else
+      render json: {
+        errors: ['No user found']
+      }
+    end
+  end
+  
+  def create
+    user = User.create!(user_params)
+    if user.save
+      render json: {
+        status: :created,
+        user: user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: user.errors.full_messages
+      }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
