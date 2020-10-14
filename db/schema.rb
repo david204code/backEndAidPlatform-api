6fad34e8ef649a6e12c12cb0108c44abbd274961
David Wu <david204code@gmail.com>
2020-10-14 23:00:32 +0100

    rails generate migration AddUserIdToMessages user:references & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_215516) do
+ActiveRecord::Schema.define(version: 2020_10_14_215851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,9 @@ ActiveRecord::Schema.define(version: 2020_10_14_215516) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "conversation_id"
+    t.bigint "user_id"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -88,4 +90,5 @@ ActiveRecord::Schema.define(version: 2020_10_14_215516) do
   add_foreign_key "conversations", "accepted_helps"
   add_foreign_key "helps", "users"
   add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
