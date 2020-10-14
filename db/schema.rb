9e4a182373f45b410f09d0e35cc6a4e44ec4b112
David Wu <david204code@gmail.com>
2020-10-14 22:57:51 +0100

    rails generate migration AddConversationIdToMessages conversation:references & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_214759) do
+ActiveRecord::Schema.define(version: 2020_10_14_215516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2020_10_14_214759) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "conversation_id"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -85,4 +87,5 @@ ActiveRecord::Schema.define(version: 2020_10_14_214759) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "conversations", "accepted_helps"
   add_foreign_key "helps", "users"
+  add_foreign_key "messages", "conversations"
 end
