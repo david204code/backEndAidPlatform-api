7db9234fc3a122001c139ddcda0fa07bca80eb78
David Wu <david204code@gmail.com>
2020-10-14 22:35:31 +0100

    rails db:migrate for the conversations table with the title attribute

--- db/migrate/20201014212758_create_conversations.rb
+++ db/migrate/20201014212758_create_conversations.rb
@@ -1,7 +1,8 @@
 class CreateConversations < ActiveRecord::Migration[6.0]
   def change
     create_table :conversations do |t|
-
+      t.string :title
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_131348) do
+ActiveRecord::Schema.define(version: 2020_10_14_212758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2020_10_14_131348) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "helps", force: :cascade do |t|
